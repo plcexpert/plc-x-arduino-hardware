cce67d0033044e397687be07fea0029a8885b185
support <support@plc-expert.com>
2019-07-03 21:39:25 +0200

    Add powershell script to simplify pull changes

---
+++ Git pull Plc-Expert repositories.ps1
@@ -0,0 +1,35 @@
+﻿# PLC-EXAMPLES
+Write-Host -NoNewLine 'Pull PLC-EXPERT Examples : ';
+cd plc-examples\
+git pull
+cd..
+
+# PLC-Hardware support package for Arduino
+Write-Host -NoNewLine 'Pull PLC-EXPERT Arduino Hardware : ';
+cd plc-expert-arduino-hardware\
+git pull
+cd..
+
+# PLC-LAB
+Write-Host -NoNewLine 'Pull PLC-EXPERT LAB : ';
+cd PlcLab\
+git pull
+cd..
+
+# PLC-LAB
+Write-Host -NoNewLine 'Pull PLC-EXPERT Visuino Examples : ';
+cd PlcVisuinoExamples\
+git pull
+cd..
+
+# PLC-WISP
+Write-Host -NoNewLine 'Pull PLC-WISP code : ';
+cd Plc-Wisp\
+git pull
+cd..
+
+
+# END
+Write-Host -NoNewLine 'END, Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
